aecb50663cdccb741e318ac87eaf7dd53c1adb7c
naveen kumar <navee@MacBook-Pro.local>
2026-07-23 16:13:00 -0400

    Correct Janvika birthday age

--- frontend/src/partyConfig.js
+++ frontend/src/partyConfig.js
@@ -2,8 +2,8 @@
 export const party = {
   childName: 'Janvika Chowdary',
   headline: 'Our Little Angel',
-  age: 6,
-  birthday: 'July 26, 2026',
+  age: 5,
+  birthday: 'July 25, 2021',
   celebrationDate: 'Sunday, July 26, 2026',
   time: '12:00 PM',
   venue: 'Scioto Park, Scioto South Shelter',
@@ -11,5 +11,5 @@ export const party = {
   mapUrl: 'https://www.google.com/maps/search/?api=1&query=7377+Riverside+Dr%2C+Dublin%2C+OH+43017',
   about: 'She fills every day with bright questions, big hugs, and the happiest little dances. We would love for you to help us make her birthday extra special.',
   hostNames: 'With love, her family',
-  birthdayNote: 'Her birthday celebration begins at noon on Sunday, July 26. We would love to celebrate this joyful day with you!',
+  birthdayNote: 'Janvika turns five on Saturday, July 25, and her birthday celebration begins at noon on Sunday, July 26. We would love to celebrate this joyful day with you!',
 }
